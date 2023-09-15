96f5831e98f0e666778b2dc15a1434e2fe00a79b
victorpedroso <victor1pedroso@gmail.com>
2023-09-15 17:50:36 -0300

    Trabalhando com ejs

---
+++ Aula 04/app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+
+var app = express();
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+    res.render('site/home');
+});
+
+app.get('/contato', function(req, res) {
+    res.render('site/contato');
+});
+
+var porta = 3000;
+app.listen(porta, function(){
+    console.log('Servidor rodando na porta ' + porta);
+});
